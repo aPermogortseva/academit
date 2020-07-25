60338bfcde141135e654b448a46f9d0a0625dcb3
aPermogortseva <dm3gGjPZQ67Tmke>
2020-07-25 15:42:36 +0700

    добавлена вторая часть задачи Range

--- Range/src/aPermRange/Main.java
+++ Range/src/aPermRange/Main.java
@@ -1,12 +1,29 @@
 package aPermRange;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(3.7, 69);
+        Range leftRange = new Range(3.7, 69);
+
+        leftRange.setFrom(4.1);
+        leftRange.setTo(70);
+
+        System.out.println("Начало левого диапазона = " + leftRange.getFrom());
+        System.out.println("Конец левого диапазона = " + leftRange.getTo());
+
 
         double point = 4.5;
 
-        System.out.println("Точка лежит в диапазоне: " + range.isInside(point));
-        System.out.println("Длина диапазона = " + range.getLength());
+        System.out.println("Точка лежит в левом диапазоне: " + leftRange.isInside(point));
+        System.out.println("Длина левого диапазона = " + leftRange.getLength());
+
+        Range rightRange = new Range(70, 80);
+
+        System.out.println("Пересечение диапазонов: " + leftRange.getIntersection(rightRange));
+
+        System.out.println("Объединение множеств: " + Arrays.toString(leftRange.getUnionRange(rightRange)));
+
+        System.out.println("Разность множеств: " + Arrays.toString(leftRange.getComplementRange(rightRange)));
     }
 }
--- Range/src/aPermRange/Range.java
+++ Range/src/aPermRange/Range.java
@@ -32,4 +32,54 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public Range getIntersection(Range rightRange) {
+        if (this.to > rightRange.from && this.to < rightRange.to) {
+            return new Range(rightRange.from, this.to);
+        }
+
+        if (rightRange.from < this.to && rightRange.to < this.to) {
+            return new Range(rightRange.from, rightRange.to);
+        }
+
+        return null;
+    }
+
+    public Range[] getUnionRange(Range rightRange) {
+        if (this.to >= rightRange.from) {
+            Range[] unionRange = new Range[1];
+            unionRange[0] = new Range(this.from, rightRange.to);
+
+            return unionRange;
+        }
+        Range[] unionRange = new Range[2];
+        unionRange[0] = new Range(this.from, this.to);
+        unionRange[1] = new Range(rightRange.from, rightRange.to);
+
+        return unionRange;
+    }
+
+    public Range[] getComplementRange(Range rightRange) {
+        if (this.to > rightRange.to) {
+            Range[] complementRange = new Range[2];
+            complementRange[0] = new Range(this.from, rightRange.from);
+            complementRange[1] = new Range(rightRange.to, this.to);
+
+            return complementRange;
+        }
+
+        if (rightRange.to > this.to && rightRange.from < this.to) {
+            Range[] complementRange = new Range[1];
+            complementRange[0] = new Range(this.from, rightRange.from);
+
+            return complementRange;
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return from + " , " + to;
+    }
 }
